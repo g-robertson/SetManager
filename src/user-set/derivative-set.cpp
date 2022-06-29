7fbe4ef3bd3ac4e154a0dc8e8086d553fc3eacd8
Gregory Robertson <robertson.gregory.a@gmail.com>
2022-06-29 12:44:22 -0400

    Add derivative set interface

---
+++ src/user-set/derivative-set.cpp
@@ -0,0 +1,29 @@
+/*
+    derivative-set.cpp
+
+    DerivativeSet is a set that holds the properties of a subset, while also requiring elements of certain subsets of its parent set
+    It is an abstract type of set due to not having any defined methods for gathering elements, complement elements, or whom it derives from
+    Its purpose is to provide an interface for ensuring that all subsets necessary for a derivative subset are available before finishing its load
+*/
+#include "derivative-set.hpp"
+
+DerivativeSet::DerivativeSet(UserSet* parent, const std::string& name) noexcept
+    : SubSet(parent, name)
+{}
+
+const std::unordered_map<std::string, UserSet*>& DerivativeSet::derivesFrom() const noexcept {
+    return derivesFrom_;
+}
+
+bool DerivativeSet::postParentLoad() noexcept {
+    for (auto& subset : derivesFrom()) {
+        if (subset.second->postParentLoad()) {
+            return true;
+        }
+    }
+    return postPostParentLoad();
+}
+
+bool DerivativeSet::postPostParentLoad() noexcept {
+    return false;
+}
---
+++ src/user-set/derivative-set.hpp
@@ -0,0 +1,24 @@
+/*
+    derivative-set.hpp
+
+    DerivativeSet is a set that holds the properties of a subset, while also requiring elements of certain subsets of its parent set
+    It is an abstract type of set due to not having any defined methods for gathering elements, complement elements, or whom it derives from
+    Its purpose is to provide an interface for ensuring that all subsets necessary for a derivative subset are available before finishing its load
+*/
+#pragma once
+
+#include "subset.hpp"
+
+#include <unordered_map>
+#include <set>
+
+class DerivativeSet : public SubSet {
+    public:
+        DerivativeSet(UserSet* parent, const std::string& name) noexcept;
+
+        const std::unordered_map<std::string, UserSet*>& derivesFrom() const noexcept;
+        bool postParentLoad() noexcept override;
+        virtual bool postPostParentLoad() noexcept;
+    private:
+        std::unordered_map<std::string, UserSet*> derivesFrom_;
+}; 
