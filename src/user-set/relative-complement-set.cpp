9ef6b41342516f75cb3c91ca4767877126e27d58
Gregory Robertson <robertson.gregory.a@gmail.com>
2022-07-04 22:05:09 -0400

    Add relative complement set

---
+++ src/user-set/relative-complement-set.cpp
@@ -0,0 +1,95 @@
+/*
+    relative-complement-set.hpp
+    RelativeComplement set is a derivative set that holds the relative complement of an existing subset relative to the parent
+*/ 
+#include "relative-complement-set.hpp"
+
+#include <algorithm>
+
+RelativeComplementSet::RelativeComplementSet(UserSet* parent, const std::string& name, UserSet* set) noexcept
+    : DerivativeSet(parent, name, {set})
+{}
+
+RelativeComplementSet::RelativeComplementSet(UserSet* parent, const std::string& name) noexcept
+    : DerivativeSet(parent, name)
+{}
+
+UserSet* RelativeComplementSet::createSet(UserSet& parent, const std::string& name) noexcept {
+    auto* set = parent.queryForSubset();
+    if (set == nullptr) {
+        return nullptr;
+    }
+
+    return new RelativeComplementSet(&parent, name, set);
+}
+
+const std::set<std::string>* RelativeComplementSet::elements() noexcept {
+    const auto* parentElements = parent_->elements();
+    const auto* setElements = derivesFrom()[0]->elements();
+    // the only infinite relative complement set occurs when both parent set is infinite, and subset is finite
+    if (parentElements == nullptr && setElements != nullptr) {
+        return nullptr;
+    }
+
+    output.clear();
+    if (parentElements == nullptr && setElements == nullptr) {
+        const auto* parentComplementElements = parent_->complementElements();
+        const auto* setComplementElements = derivesFrom()[0]->complementElements();
+        // https://proofwiki.org/wiki/Definition:Relative_Complement
+        // RelativeComplement(A, B) = A Difference B
+        // https://proofwiki.org/wiki/Set_Difference_of_Complements
+        // Complement(B) Difference Complement(A) = A Difference B
+        // => A Difference B = Complement(B) Difference Complement(A)
+        // => RelativeComplement(A, B) = Complement(B) Difference Complement(A)
+        std::set_difference(
+            setComplementElements->begin(), setComplementElements->end(),
+            parentComplementElements->begin(), parentComplementElements->end(),
+            std::inserter(output, output.begin())
+        );
+    // parentElements and setElements must be finite at this point, therefore, sanity checks
+    } else if (parentElements == nullptr) {
+        std::cout << "[FATAL ERROR]: A condition where a parent has infinite elements despite passing the only infinite element case should never happen.\n";
+        exitProgram();
+    } else if (setElements == nullptr) {
+        std::cout << "[FATAL ERROR]: A condition where a subset of parent has infinite elements where the parent has finite should never happen.\n";
+        exitProgram();
+    // parentElements and setElements REALLY must be finite at this point
+    } else {
+        // https://proofwiki.org/wiki/Definition:Relative_Complement
+        // RelativeComplement(A, B) = A Difference B
+        std::set_difference(
+            parentElements->begin(), parentElements->end(),
+            setElements->begin(), setElements->end(),
+            std::inserter(output, output.begin())
+        );
+    }
+
+    return &output;
+}
+
+const std::set<std::string>* RelativeComplementSet::complementElements() noexcept {
+    const auto* parentComplementElements = parent_->complementElements();
+    const auto* setElements = derivesFrom()[0]->elements();
+    // the only infinite relative complement set occurs when both parent set is infinite, and subset is finite
+    if (parentComplementElements != nullptr || setElements == nullptr) {
+        return nullptr;
+    }
+
+    output.clear();
+    // https://proofwiki.org/wiki/Set_Difference_as_Intersection_with_Complement
+    // A Difference B = A Intersect Complement(B)
+    // => Complement(A Difference B) = Complement(A Intersect Complement(B))
+    // https://proofwiki.org/wiki/De_Morgan%27s_Laws_(Set_Theory)/Set_Complement/Complement_of_Union
+    // Complement(A Intersect B) = Complement(A) Union Complement(B)
+    // => Complement(A Difference B) = Complement(A) Union B
+    // https://proofwiki.org/wiki/Definition:Relative_Complement
+    // RelativeComplement(A, B) = A Difference B
+    // => Complement(RelativeComplement(A, B)) = Complement(A) Union B
+    std::set_union(
+        parentComplementElements->begin(), parentComplementElements->end(),
+        setElements->begin(), setElements->end(),
+        std::inserter(output, output.begin())
+    );
+
+    return &output;
+}
---
+++ src/user-set/relative-complement-set.hpp
@@ -0,0 +1,27 @@
+/*
+    relative-complement-set.hpp
+    RelativeComplement set is a derivative set that holds the relative complement of an existing subset relative to the parent
+*/ 
+#pragma once
+
+#include "derivative-set.hpp"
+
+class RelativeComplementSet : public DerivativeSet {
+    public:
+        RelativeComplementSet(UserSet* parent, const std::string& name, UserSet* set) noexcept;
+        RelativeComplementSet(UserSet* parent, const std::string& name) noexcept;
+    
+        // #region SubSet public members override 
+        static UserSet* createSet(UserSet& parent, const std::string& name) noexcept;
+        // #endregion 
+        // #region UserSet public members override 
+        static constexpr char type_ = 'C';
+        char type() const noexcept override { return type_; }
+    
+        const std::set<std::string>* elements() noexcept override;
+        const std::set<std::string>* complementElements() noexcept override;
+        // #endregion 
+
+    private:
+        mutable std::set<std::string> output;
+};
